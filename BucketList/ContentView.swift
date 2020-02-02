471f148da344403dc0f96ea1a089db9ba149d17b
s4rv4d <sarvadshetty@gmail.com>
2020-02-02 07:32:27 +0530

    Initial Commit

---
+++ BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BucketList
+//
+//  Created by Sarvad shetty on 2/2/20.
+//  Copyright © 2020 Sarvad shetty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
