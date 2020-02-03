41e3b02b66919c5e4ab58944478f2d294eeac2a8
s4rv4d <sarvadshetty@gmail.com>
2020-02-03 06:51:20 +0530

    testing out file manager

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -11,11 +11,35 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, World!")
+            .onTapGesture {
+                let str = "Text message"
+                let url2 = FileManager.default.getUrl("message.txt")
+                let url = self.getDocumentDirectory().appendingPathComponent("message.txt")
+                do {
+                    try str.write(to: url2, atomically: true, encoding: .utf8)
+                    let input = try String(contentsOf: url2)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+        }
+    }
+    
+    func getDocumentDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension FileManager {
+    func getUrl(_ appendingFilePath: String) -> URL {
+        let url = self.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        return url.appendingPathComponent(appendingFilePath)
     }
 }
