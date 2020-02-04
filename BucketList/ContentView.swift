46f447275024c6b586932d97aaaf9ab868100773
s4rv4d <sarvadshetty@gmail.com>
2020-02-04 12:32:16 +0530

    tested out auth

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,13 +6,48 @@
 //  Copyright © 2020 Sarvad shetty. All rights reserved.
 //
 
+
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
     
+    @State private var isUnlocked = false
+    
     var body: some View {
-        MapView()
-            .edgesIgnoringSafeArea(.all)
+        VStack {
+            if self.isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }.onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        //create and LAContext object
+        let context = LAContext()
+        //creating an optional error variable of type NSError cause this LA was built using objc
+        var error: NSError?
+        
+        //check if biometric auth can be possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                //need to push to main thread to make UI Updates
+                DispatchQueue.main.async {
+                    if success {
+                        //auth successfully
+                        //use @State var
+                        self.isUnlocked = true
+                    } else {
+                        //there was a problem
+                    }
+                }
+            }
+        } else {
+            //no biometric system available
+        }
     }
 }
 
